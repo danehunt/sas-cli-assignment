8c0fc733a1f15015bbde224fc4bcd2f21ffa4490
danehunt <104741736+danehunt@users.noreply.github.com>
2022-05-02 16:38:44 -0400

    Add files via upload

--- SAS_CodingAssignment.py
+++ SAS_CodingAssignment.py
@@ -5,8 +5,12 @@ Created on Sat Apr 30 15:19:32 2022
 @author: Dane Hunt
 """
 
+import base64
 from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
+# lists the possible commands and prompts the user for their command
 def getInput():
     print()
     print("Commands:")
@@ -15,42 +19,46 @@ def getInput():
     print("Q - Quit")
     return input("Enter a command: ")
 
+# loads a file and converts the contents of the file to a string
 def loadFile():
     fileLoaded = False
     while not fileLoaded:
         filename = input("Enter the name of a file in you current working directory (ex: file.txt) or Q to quit: ")
         try:
             file = open(filename, "rb")
-            message = file.read()
+            textInFile = file.read()
             file.close()
         except:
-            if filename == "Q":
+            if filename.upper() == "Q":
                 return None
             print("File name " + filename + " does not exist.")
         else:
             print("Successfully loaded " + filename + "!")
             fileLoaded = True
-    return message
+    return textInFile
 
-def saveFile(message):
+# saves text within a file of a name that is entered
+def saveFile(textInFile):
     filename = input("What would you like to save the new file as? (ex: file.txt): ")
     file = open(filename, "wb")
-    file.write(message)
+    file.write(textInFile)
     file.close()
 
+# encrypts a file using the current key
 def encryptFile(f):
-    message = loadFile()
-    if message != None:
-        encrypted = f.encrypt(message)
+    textInFile = loadFile()
+    if textInFile != None:
+        encrypted = f.encrypt(textInFile)
         print("File has been encrypted.")
         saveFile(encrypted)
         return encrypted
 
+# decrypts a file using the current key
 def decryptFile(f):
-    message = loadFile()
-    if message != None:
+    textInFile = loadFile()
+    if textInFile != None:
         try:
-            decrypted = f.decrypt(message)
+            decrypted = f.decrypt(textInFile)
         except:
             print("File could not be decrypted with key.")
         else:
@@ -58,17 +66,44 @@ def decryptFile(f):
             saveFile(decrypted)
             return decrypted
 
+# main method
+# ----------------------------------------------------------
 print("Welcome to the File Encryption CLI!")
 print("-----------------------------------")
-key = Fernet.generate_key()
+
+# get password and encode in bytes
+password = input("Please input your password: ").encode()
+
+# used hardcoded salt for example purposes
+# salt = os.urandom(16) in order to generate randomly
+salt = b'\xdc\xe2L\xcf\x12\xceAbP\x88\xe1\xc1\xbb4\x05\x8f'
+
+# key derivation function
+# algorithm: hash algorithm used to generate key
+# length: length of key in bytes
+# salt: typically randomly generated and stored in database
+# iterations: 390000 is current recommended # of iterations
+kdf = PBKDF2HMAC(
+    algorithm=hashes.SHA256(),
+    length=32,
+    salt=salt,
+    iterations=390000,
+)
+
+# create key based on password input
+key = base64.urlsafe_b64encode(kdf.derive(password))
+
+# create Fernet object using created key
 f = Fernet(key)
+
+# runs until user chooses to quit the program
 user_input = ""
-while user_input != "Q":
+while user_input.upper() != "Q":
     user_input = getInput()
-    if user_input == "E":
+    if user_input.upper() == "E":
         encryptFile(f)
-    elif user_input == "D":
+    elif user_input.upper() == "D":
         decryptFile(f)
-    elif user_input != "Q":
+    elif user_input.upper() != "Q":
         print("NOT A VAILD COMMAND, PLEASE TRY AGAIN")
 
